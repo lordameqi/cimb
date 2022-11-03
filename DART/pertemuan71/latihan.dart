0ba7d90a6872adf08a0b85e18ef741dcb2c4318a
rachmad <rachmad.fadillah@students.uin-suska.ac.id>
2022-11-03 13:07:21 +0700

    dart

---
+++ DART/pertemuan71/latihan.dart
@@ -0,0 +1,114 @@
+void main() {
+  // String name = 'Hai';
+  // print(name.runtimeType);
+
+  // num numa = 10.1;
+  // num numb = 10;
+  // String name = 'hai';
+  // print(numa.runtimeType);
+  // print(numb.runtimeType);
+  // print(name.runtimeType);
+
+  // String name = 'joey';
+  // int age = 23;
+  // double grades = 9.89;
+
+  // print("hi there, my name is '$name'");
+  // print('I\'m \'${age}\' year old, and  i\'ve received \'${grades*10}\'score');
+
+  // bool isOk = true;
+  // bool isNotOk = false;
+
+  // print("isOk value : $isOk , is not ok value : $isNotOk");
+  // print("isOk value : ${isOk.runtimeType} , is not ok value : ${isNotOk.runtimeType}");
+  
+  // Object name = "airell";
+  // Object age = 23;
+  // Object stilYoung = true;
+
+  // print("name type: ${name.runtimeType}");
+  // print("age type: ${age.runtimeType}");
+  // print("still young type: ${stilYoung.runtimeType}");
+
+  // String myName = name as String;
+
+  // print(myName);
+
+  // List <String> names = ['airell','joey','lana'];
+  // List <int> ages = [23,24,25];
+  // List <Object> randomData = ['yosi',23,true];
+
+  // print(ages);
+  // print("names length: `${names.length}` ");
+  // print("first index: ${names[0]}");
+  // print("last index : ${names[names.length - 1]}");
+
+  // names.add("hermes");
+  // print(names);
+
+  // names[2]= 'jhon';
+  // print(names);
+
+  // names.remove('joey');
+  // print(names);
+
+  // List<String>hasNumber= ["#1","#2","#3","#4"];
+  // List<String>newHasNumber = ["#0", ...hasNumber, "#5"];
+  // print(newHasNumber);
+
+//   Map<String, int> foodPrice ={
+//     "almond" : 2000,
+//     "banana" :  3000,
+//     "apple" : 4000,
+//     'steak' :10000
+//   };
+
+//   Map<String, Object> person ={
+//     "name":"airell jordan",
+//     "age" : 23,
+//     "address": "jalan sudirman"
+//   };
+
+//   print(foodPrice['almond']);
+//   print(foodPrice['Almond']);
+
+//   foodPrice['apple'] = 10;
+//   foodPrice['nanas'] = 1;
+//   print(foodPrice);
+
+// dynamic randomData =10;
+// randomData = 'irak';
+// print(randomData);
+// randomData = true;
+// print(randomData);
+// randomData = ['123',1,false];
+// print(randomData);
+// randomData = {
+// 'apple' : 1,
+// 'banana' :2
+// };
+// print(randomData);
+
+// dynamic ha = 10;
+// int num = ha ;
+// print(num);
+
+
+// var name = 'airell';
+//  name = 'jordan';
+//  print(name.runtimeType);
+
+// final String name = 'airell';
+// name ='huy';
+
+// final students = ["airel"];
+// students.add("q");
+
+// print(students);
+
+int? num;
+print(num);
+
+int counter = num as int;
+print("qwqw ${counter}");
+}
